d175ee84a8b22c0be1dc6df6d3d1ad8de5924d16
Navneet Ranjan <500067951@stu.upes.ac.in>
2020-10-28 17:29:45 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -25,7 +25,7 @@
             msg1.textContent='Loading......'
             msg2.textContent='';
 
-            fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+            fetch('/weather?address='+location).then((response)=>{
 
          response.json().then((data)=>{
 
